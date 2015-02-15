5f07004b746eb9710334d1494bab8684d349c9dc
mischabarthel <mischabarthel@gmail.com>
2015-02-15 15:35:58 +0100

    Lala

---
+++ R programming/assignment1.R
@@ -0,0 +1,28 @@
+library(plyr)
+
+
+# Niet nodig! Wel leerzaam.
+extract_source <- function(file_name) {
+  md <- regexpr("[[:digit:]]{3}", file_name)
+  as.numeric(regmatches(file_name, md))
+}
+
+
+# Niet nodig! Wel leerzaam.
+read_file <- function(file_name) {
+  result <- read.csv(file_name)
+  result$monitor_id = extract_source(file_name)
+  result
+}
+
+read_files <- function() {
+  files <- list.files("datasciencecoursera//R programming/specdata/", pattern="*.csv", full=TRUE)
+  result <- ldply(files, read.csv)
+  na.omit(result)
+}
+
+rows_by_monitors <- function(monitor_ids) {
+  data[data$ID %in% monitor_ids, ]
+}
+
+
